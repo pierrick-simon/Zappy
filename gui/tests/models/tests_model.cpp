25fe76447b9f1d4d2b0b6df6bd8bbec5aaac402c
Natan Pereira <natan.pereira@epitech.eu>
2026-06-10 20:10:06 +0200

    feat(Graphics/Model): Added unit tests for parseTexturePos

--- gui/tests/models/tests_model.cpp
+++ gui/tests/models/tests_model.cpp
@@ -52,3 +52,36 @@ Test(model_parsing, vertex_w)
         cr_assert_eq(model.getVertices().front(), expected);
     });
 }
+Test(model_parsing, texture_pos_empty)
+{
+    cr_assert_any_throw(
+        { Graphics::Model model(DIRECTORY / "empty_texture_pos.obj"); });
+}
+
+Test(model_parsing, texture_pos_only_x)
+{
+    Graphics::Model::TexturePosType expected {1, 0, 0};
+    cr_assert_none_throw({
+        Graphics::Model model(DIRECTORY / "only_x_texture_pos.obj");
+        cr_assert_eq(model.getTexturePos().front(), expected);
+    });
+}
+
+Test(model_parsing, texture_pos_simple)
+{
+    Graphics::Model::TexturePosType expected {1, 2, 0};
+    cr_assert_none_throw({
+        Graphics::Model model(DIRECTORY / "simple_texture_pos.obj");
+        cr_assert_eq(model.getTexturePos().front(), expected);
+    });
+}
+
+Test(model_parsing, texture_pos_3d)
+{
+    Graphics::Model::TexturePosType expected {1, 2, 3};
+
+    cr_assert_none_throw({
+        Graphics::Model model(DIRECTORY / "3d_texture_pos.obj");
+        cr_assert_eq(model.getTexturePos().front(), expected);
+    });
+}
