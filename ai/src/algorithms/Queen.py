c3a7ee422e1adb1e6639d4f05e783734f93cba4b
rayan <rayan.ouerdane@epitech.eu>
2026-06-18 17:58:13 +0200

    refactor(ia): queen ai

---
+++ ai/src/algorithms/Queen.py
@@ -0,0 +1,13 @@
+##
+## EPITECH PROJECT, 2026
+## Zappy
+## File description:
+## Human AI
+##
+
+from src.connection_handler import ConnectionHandler
+from src.constants.constants import COMMAND_FACTORY
+
+class Queen:
+    def __init__(self, handler: ConnectionHandler) -> None:
+        self._handler = handler
