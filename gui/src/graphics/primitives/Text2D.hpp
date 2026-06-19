4d30953ba8aa3d0657a442155ab80d3476b632cc
pierrick <pierrick.simon@epitech.eu>
2026-06-19 18:00:02 +0200

    feat(gui/primitive): add a Box2D and a better Text2D class

--- gui/src/graphics/primitives/Text2D.hpp
+++ gui/src/graphics/primitives/Text2D.hpp
@@ -7,16 +7,39 @@
 
 #ifndef TEXT2D_HPP
     #define TEXT2D_HPP
-    #include <Text.hpp>
 
+    #include <memory>
+    #include <optional>
+    #include "Text.hpp"
     #include "graphics/UiObject.hpp"
 
 namespace Graphics {
 
-    class Text2D : public raylib::Text, public UiObject {
+    class Text2D : public UiObject {
     public:
-        using Text::Text;
+        Text2D() = default;
         void draw2D() const override;
+
+        void setFont(Font &font);
+        void setStr(std::string str);
+        void setPos(Vector2 pos);
+        void setOrigin(Vector2 origin);
+        void setRotation(float rotation);
+        void setFontSize(float fontSize);
+        void setSpacing(float spacing);
+        void setColor(Color color);
+
+        [[nodiscard]] Vector2 getSize() const;
+
+    private:
+        std::optional<std::reference_wrapper<Font>> _font;
+        std::string _str;
+        Vector2 _pos = {0, 0};
+        Vector2 _origin = {0, 0};
+        float _rotation = 0;
+        float _fontSize = 0;
+        float _spacing = 0;
+        Color _color = BLACK;
     };
 
 } // namespace Graphics
